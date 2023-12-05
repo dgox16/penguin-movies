d22c82f35a04419c08a2d075cf4a272592fb775c
Diego <dgox16@gmail.com>
2023-12-05 15:22:56 -0600

    Fix

--- client/src/pages/ProtectedRoutes.jsx
+++ client/src/pages/ProtectedRoutes.jsx
@@ -5,38 +5,37 @@ import { verifyTokenRequest } from "../services/usersAdministration";
 import Cookies from "js-cookie";
 
 export const ProtectedRoutes = () => {
-    // const { isAuthenticated, logout, setLoading, setToken, setUser } = useAuthStore();
-    //
-    // useEffect(() => {
-    //     const checkLogin = async () => {
-    //         const cookies = Cookies.get();
-    //         if (!cookies.token) {
-    //             console.log("Error");
-    //             logout();
-    //             setLoading(false);
-    //         }
-    //         try {
-    //             const res = await verifyTokenRequest(cookies.token);
-    //             if (!res) {
-    //                 logout();
-    //                 setLoading(false);
-    //                 return;
-    //             }
-    //             setUser(res);
-    //             setToken(res.token);
-    //             setLoading(false);
-    //         } catch (_error) {
-    //             logout();
-    //             setLoading(false);
-    //         }
-    //     };
-    //
-    //     checkLogin();
-    // }, []);
-    //
-    // if (!isAuthenticated) {
-    //     return <Navigate to="/login" replace={true} />;
-    // }
+    const { isAuthenticated, token, logout, setLoading, setToken, setUser } =
+        useAuthStore();
+
+    useEffect(() => {
+        const checkLogin = async () => {
+            if (token === "") {
+                logout();
+                setLoading(false);
+            }
+            try {
+                const res = await verifyTokenRequest(token);
+                if (!res) {
+                    logout();
+                    setLoading(false);
+                    return;
+                }
+                setUser(res);
+                setToken(res.token);
+                setLoading(false);
+            } catch (_error) {
+                logout();
+                setLoading(false);
+            }
+        };
+
+        checkLogin();
+    }, []);
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace={true} />;
+    }
 
     return (
         <div>
