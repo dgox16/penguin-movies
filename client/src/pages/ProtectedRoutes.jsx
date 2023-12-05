ace746d3c2dd5225f18847939b6e57735c3f9adc
Diego <dgox16@gmail.com>
2023-12-05 15:44:58 -0600

    ksa

--- client/src/pages/ProtectedRoutes.jsx
+++ client/src/pages/ProtectedRoutes.jsx
@@ -34,10 +34,10 @@ export const ProtectedRoutes = () => {
         checkLogin();
     }, []);
 
-    if (!isAuthenticated) {
-        console.log(isAuthenticated);
-        return <Navigate to="/login" replace={true} />;
-    }
+    // if (!isAuthenticated) {
+    //     console.log(isAuthenticated);
+    //     return <Navigate to="/login" replace={true} />;
+    // }
 
     return (
         <div>
