e5730d0b11c5b618cce2ca0a07eab0c2f7bcbb3c
Diego <dgox16@gmail.com>
2024-01-11 23:32:41 -0600

    dsadas

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://penguin-movies-frontend.onrender.com",
+        origin: "https://localhost:5173",
         credentials: true,
     }),
 );
