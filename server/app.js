5facfee9d851b8ceb26261a70413def603a62caf
Diego <dgox16@gmail.com>
2024-01-11 23:50:12 -0600

    few

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["https://localhost:5173"],
+        origin: "https://penguin-movies-frontend.onrender.com",
         credentials: true,
     }),
 );
