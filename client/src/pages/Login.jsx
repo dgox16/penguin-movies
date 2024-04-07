82cde4b722f15efad8d1c77199edc9d4bbb5f320
Diego <dgox16@gmail.com>
2024-04-07 12:16:20 -0600

    Mejoras en el login y registro

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,15 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAuthStore } from "../store/auth";
-import { Button, Card, CardBody, CardHeader, Input, Spacer } from "@nextui-org/react";
+import {
+    Link,
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    Input,
+    Spacer,
+} from "@nextui-org/react";
 import { EyeFilledIcon, EyeSlashFilledIcon } from "../assets/icons/EyeFilledIcon";
 import { usePurchasesStore } from "../store/purchases";
 import { useOrdersStore } from "../store/orders";
@@ -68,65 +76,77 @@ export const Login = () => {
             <div className="flex justify-center h-screen -mt-16 pt-16 items-center">
                 <Card className="mx-6">
                     <CardHeader className="flex items-center justify-center mt-2">
-                        <h1 className="text-4xl font-black">LOGIN</h1>
+                        <h1 className="text-5xl font-black">LOGIN</h1>
                     </CardHeader>
                     <CardBody>
-                        {loginError.map((err) => (
-                            <div
-                                className="bg-red-500 mb-3 font-bold text-center text-white rounded-md p-4"
-                                key={err}
-                            >
-                                {err}
-                            </div>
-                        ))}
-                        <form onSubmit={onSubmit}>
-                            <Input
-                                {...register("username", {
-                                    required: "This is required",
-                                })}
-                                type="text"
-                                variant="bordered"
-                                size="lg"
-                                className="font-semibold"
-                                label="USERNAME"
-                                labelPlacement={"outside"}
-                                placeholder="Enter your username"
-                                isInvalid={errors.username ? true : false}
-                                errorMessage={errors.username?.message}
-                            />
-                            <Spacer y={6} />
-                            <Input
-                                {...register("password", {
-                                    required: "This is required",
-                                })}
-                                size="lg"
-                                label="PASSWORD"
-                                className="font-semibold"
-                                variant="bordered"
-                                placeholder="Enter your password"
-                                endContent={
-                                    <button
-                                        className="focus:outline-none"
-                                        type="button"
-                                        onClick={() => setIsVisible(!isVisible)}
+                        <div className="px-3 py-6 md:px-8 md:py-12">
+                            {loginError.map((err) => (
+                                <div
+                                    className="bg-red-500 mb-3 font-bold text-center text-white rounded-md p-4"
+                                    key={err}
+                                >
+                                    {err}
+                                </div>
+                            ))}
+                            <form onSubmit={onSubmit}>
+                                <Input
+                                    {...register("username", {
+                                        required: "This is required",
+                                    })}
+                                    type="text"
+                                    variant="bordered"
+                                    size="lg"
+                                    className="font-semibold"
+                                    label="USERNAME"
+                                    labelPlacement={"outside"}
+                                    placeholder="Enter your username"
+                                    isInvalid={errors.username ? true : false}
+                                    errorMessage={errors.username?.message}
+                                />
+                                <Spacer y={6} />
+                                <Input
+                                    {...register("password", {
+                                        required: "This is required",
+                                    })}
+                                    size="lg"
+                                    label="PASSWORD"
+                                    className="font-semibold"
+                                    variant="bordered"
+                                    placeholder="Enter your password"
+                                    endContent={
+                                        <button
+                                            className="focus:outline-none"
+                                            type="button"
+                                            onClick={() => setIsVisible(!isVisible)}
+                                        >
+                                            {isVisible ? (
+                                                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                            ) : (
+                                                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                            )}
+                                        </button>
+                                    }
+                                    type={isVisible ? "text" : "password"}
+                                    labelPlacement={"outside"}
+                                    isInvalid={errors.password ? true : false}
+                                    errorMessage={errors.password?.message}
+                                />
+                                <Spacer y={4} />
+                                <div className="flex justify-end">
+                                    <Link
+                                        className="italic"
+                                        href="/register"
+                                        color="success"
                                     >
-                                        {isVisible ? (
-                                            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                                        ) : (
-                                            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                                        )}
-                                    </button>
-                                }
-                                type={isVisible ? "text" : "password"}
-                                labelPlacement={"outside"}
-                                isInvalid={errors.password ? true : false}
-                                errorMessage={errors.password?.message}
-                            />
-                            <Spacer y={8} />
-                            <Button type="submit" className="w-full" color="success">
-                                LOGIN
-                            </Button>
-                        </form>
+                                        Or create a new account
+                                    </Link>
+                                </div>
+                                <Spacer y={4} />
+                                <Button type="submit" className="w-full" color="success">
+                                    Login
+                                </Button>
+                            </form>
+                        </div>
                     </CardBody>
                 </Card>
             </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,15 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAuthStore } from "../store/auth";
-import { Button, Card, CardBody, CardHeader, Input, Spacer } from "@nextui-org/react";
+import {
+    Button,
+    Link,
+    Card,
+    CardBody,
+    CardHeader,
+    Input,
+    Spacer,
+} from "@nextui-org/react";
 import { getOrders, getPurchases, getShoppingCart } from "../functions/getData";
 import { usePurchasesStore } from "../store/purchases";
 import { useOrdersStore } from "../store/orders";
@@ -66,95 +74,107 @@ export const Register = () => {
             <div className="flex justify-center h-screen -mt-16 pt-16 items-center">
                 <Card className="mx-6">
                     <CardHeader className="flex items-center justify-center mb-5 mt-2">
-                        <h1 className="text-4xl font-black">REGISTER</h1>
+                        <h1 className="text-4xl md:text-5xl font-black">REGISTER</h1>
                     </CardHeader>
                     <CardBody>
-                        {registerError.map((err) => (
-                            <div
-                                className="bg-red-500 mb-3 font-bold text-center text-white rounded-md p-4"
-                                key={err}
-                            >
-                                {err}
-                            </div>
-                        ))}
-                        <form onSubmit={onSubmit}>
-                            <Input
-                                {...register("username", {
-                                    required: "This is required",
-                                })}
-                                type="text"
-                                variant="bordered"
-                                size="lg"
-                                label="CREATE A NEW USER"
-                                className="font-semibold"
-                                labelPlacement={"outside"}
-                                placeholder="Enter your username"
-                                isInvalid={errors.username ? true : false}
-                                errorMessage={errors.username?.message}
-                            />
-                            <Spacer y={6} />
-                            <Input
-                                {...register("password", {
-                                    required: "This is required",
-                                })}
-                                size="lg"
-                                label="CREATE A NEW PASSWORD"
-                                variant="bordered"
-                                className="font-semibold"
-                                placeholder="Enter your password"
-                                endContent={
-                                    <button
-                                        className="focus:outline-none"
-                                        type="button"
-                                        onClick={() => setIsVisible(!isVisible)}
+                        <div className="px-3 py-6 md:px-8 md:py-12">
+                            {registerError.map((err) => (
+                                <div
+                                    className="bg-red-500 mb-3 font-bold text-center text-white rounded-md p-4"
+                                    key={err}
+                                >
+                                    {err}
+                                </div>
+                            ))}
+                            <form onSubmit={onSubmit}>
+                                <Input
+                                    {...register("username", {
+                                        required: "This is required",
+                                    })}
+                                    type="text"
+                                    variant="bordered"
+                                    size="lg"
+                                    label="CREATE A NEW USER"
+                                    className="font-semibold"
+                                    labelPlacement={"outside"}
+                                    placeholder="Enter your username"
+                                    isInvalid={errors.username ? true : false}
+                                    errorMessage={errors.username?.message}
+                                />
+                                <Spacer y={6} />
+                                <Input
+                                    {...register("password", {
+                                        required: "This is required",
+                                    })}
+                                    size="lg"
+                                    label="CREATE A NEW PASSWORD"
+                                    variant="bordered"
+                                    className="font-semibold"
+                                    placeholder="Enter your password"
+                                    endContent={
+                                        <button
+                                            className="focus:outline-none"
+                                            type="button"
+                                            onClick={() => setIsVisible(!isVisible)}
+                                        >
+                                            {isVisible ? (
+                                                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                            ) : (
+                                                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                            )}
+                                        </button>
+                                    }
+                                    type={isVisible ? "text" : "password"}
+                                    labelPlacement={"outside"}
+                                    isInvalid={errors.password ? true : false}
+                                    errorMessage={errors.password?.message}
+                                />
+                                <Spacer y={6} />
+                                <Input
+                                    {...register("firstName", {
+                                        required: "This is required",
+                                    })}
+                                    type="text"
+                                    variant="bordered"
+                                    size="lg"
+                                    className="font-semibold"
+                                    label="WHAT IS YOUR FIRST NAME?"
+                                    labelPlacement={"outside"}
+                                    placeholder="Enter your first name"
+                                    isInvalid={errors.firstName ? true : false}
+                                    errorMessage={errors.firstName?.message}
+                                />
+                                <Spacer y={6} />
+                                <Input
+                                    {...register("lastName", {
+                                        required: "This is required",
+                                    })}
+                                    type="text"
+                                    variant="bordered"
+                                    size="lg"
+                                    className="font-semibold"
+                                    label="WHAT IS YOUR LAST NAME?"
+                                    labelPlacement={"outside"}
+                                    placeholder="Enter your last name"
+                                    isInvalid={errors.lastName ? true : false}
+                                    errorMessage={errors.lastName?.message}
+                                />
+                                <Spacer y={4} />
+                                <div className="flex justify-end">
+                                    <Link
+                                        className="italic"
+                                        href="/login"
+                                        color="warning"
                                     >
-                                        {isVisible ? (
-                                            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                                        ) : (
-                                            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                                        )}
-                                    </button>
-                                }
-                                type={isVisible ? "text" : "password"}
-                                labelPlacement={"outside"}
-                                isInvalid={errors.password ? true : false}
-                                errorMessage={errors.password?.message}
-                            />
-                            <Spacer y={6} />
-                            <Input
-                                {...register("firstName", {
-                                    required: "This is required",
-                                })}
-                                type="text"
-                                variant="bordered"
-                                size="lg"
-                                className="font-semibold"
-                                label="WHAT IS YOUR FIRST NAME?"
-                                labelPlacement={"outside"}
-                                placeholder="Enter your first name"
-                                isInvalid={errors.firstName ? true : false}
-                                errorMessage={errors.firstName?.message}
-                            />
-                            <Spacer y={6} />
-                            <Input
-                                {...register("lastName", {
-                                    required: "This is required",
-                                })}
-                                type="text"
-                                variant="bordered"
-                                size="lg"
-                                className="font-semibold"
-                                label="WHAT IS YOUR LAST NAME?"
-                                labelPlacement={"outside"}
-                                placeholder="Enter your last name"
-                                isInvalid={errors.lastName ? true : false}
-                                errorMessage={errors.lastName?.message}
-                            />
-                            <Spacer y={8} />
-                            <Button type="submit" className="w-full" color="warning">
-                                REGISTER
-                            </Button>
-                        </form>
+                                        Or sign in to your account
+                                    </Link>
+                                </div>
+                                <Spacer y={4} />
+                                <Button type="submit" className="w-full" color="warning">
+                                    Register
+                                </Button>
+                            </form>
+                        </div>
                     </CardBody>
                 </Card>
             </div>
