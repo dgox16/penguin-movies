639f90786aecc81fe8996b87ae7fa95c75f98067
Diego <dgox16@gmail.com>
2024-04-08 14:48:10 -0600

    No es necesario hacer la query para obtener el usuario, ya lo tengo

--- server/middleware/checkUser.js
+++ server/middleware/checkUser.js
@@ -1,6 +1,5 @@
 import { SECRET } from "../envConfig.js";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
 
 export const authRequired = (req, res, next) => {
     const { token } = req.cookies;
@@ -9,28 +8,29 @@ export const authRequired = (req, res, next) => {
         return res.status(401).json({ error: "Token Missing" });
     }
 
-    jwt.verify(token, SECRET, async (err, user) => {
+    jwt.verify(token, SECRET, (err, user) => {
         if (err) {
             return res.status(403).json({ error: "Token invalid" });
         }
-        req.user = await User.findById(user.id);
+        req.user = user;
         next();
     });
 };
 
 export const adminRequired = (req, res, next) => {
     const { token } = req.cookies;
+
     if (!token) {
         return res.status(401).json({ error: "Token Missing" });
     }
-    jwt.verify(token, SECRET, async (err, user) => {
+    jwt.verify(token, SECRET, (err, user) => {
         if (err) {
             return res.status(403).json({ error: "Token invalid" });
         }
         if (!user.isAdmin) {
             return res.status(403).json({ error: "Token invalid" });
         }
-        req.user = await User.findById(user.id);
+        req.user = user;
         next();
     });
 };
