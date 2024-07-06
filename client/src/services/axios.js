45c632fd9aa9be3234fb622e420538e0f2d693ad
Diego Armando <dgox16@gmail.com>
2024-07-05 22:06:28 -0600

    Ahora deploy en railway

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const baseUrl =
-    process.env.NODE_ENV === "production"
-        ? "https://api-penguin-movies.onrender.com/api/"
-        : "http://localhost:3000/api";
+	process.env.NODE_ENV === "production"
+		? "https://api-production-80eb.up.railway.app/api/"
+		: "http://localhost:3000/api";
 
 const instance = axios.create({
-    baseURL: baseUrl,
-    withCredentials: true,
+	baseURL: baseUrl,
+	withCredentials: true,
 });
 
 export default instance;
--- server/app.js
+++ server/app.js
@@ -12,40 +12,40 @@ import purchasesRoutes from "./routes/purchases.routes.js";
 const app = express();
 
 const urlFrontend =
-    process.env.NODE_ENV === "production"
-        ? "https://penguin-movies.vercel.app"
-        : "http://localhost:5173";
+	process.env.NODE_ENV === "production"
+		? "https://client-production-d073.up.railway.app/"
+		: "http://localhost:5173";
 
 app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (origin == urlFrontend) {
-                callback(null, true);
-            } else {
-                callback(new Error("Acceso no permitido para este origen"), false);
-            }
-        },
-        credentials: true,
-    }),
+	cors({
+		origin: (origin, callback) => {
+			if (origin == urlFrontend) {
+				callback(null, true);
+			} else {
+				callback(new Error("Acceso no permitido para este origen"), false);
+			}
+		},
+		credentials: true,
+	}),
 );
 app.use((err, req, res, next) => {
-    if (err instanceof Error) {
-        res.status(403).json({
-            success: false,
-            message: err.message,
-        });
-    } else {
-        next();
-    }
+	if (err instanceof Error) {
+		res.status(403).json({
+			success: false,
+			message: err.message,
+		});
+	} else {
+		next();
+	}
 });
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(
-    fileupload({
-        useTempFiles: true,
-        tempFileDir: "./upload",
-    }),
+	fileupload({
+		useTempFiles: true,
+		tempFileDir: "./upload",
+	}),
 );
 
 app.use(cookieParser());
