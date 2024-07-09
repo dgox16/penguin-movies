c75466e6adf26fd1a60efb72b3ec81e665060b5f
Diego Armando <dgox16@gmail.com>
2024-07-09 11:28:56 -0600

    Hola

--- server/db.js
+++ server/db.js
@@ -2,10 +2,16 @@ import mongoose from "mongoose";
 import { MONGODB_URI } from "./envConfig.js";
 
 export async function connectedDb() {
-	try {
-		const db = await mongoose.connect(MONGODB_URI, { autoIndex: false });
-		console.info("Connected to ", db.connection.name);
-	} catch (error) {
-		console.error(error);
+	if (!mongoose.connection.readyState) {
+		try {
+			const db = await mongoose.connect(MONGODB_URI, {
+				autoIndex: false,
+				maxPoolSize: 4,
+			});
+
+			console.info("Connected to ", db.connection.name);
+		} catch (error) {
+			console.error(error);
+		}
 	}
 }
