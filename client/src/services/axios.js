3530027c55ae72b7a55ed45c9b27bd0f4b8b62e2
Diego Armando <dgox16@gmail.com>
2024-07-09 16:20:38 -0600

    Se cambio el dominio de la api

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://api-production-80eb.up.railway.app/api/"
+		? "https://penguin-movies-production.up.railway.app/api/"
 		: "http://localhost:3000/api";
 
 const instance = axios.create({
