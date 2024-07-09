b7ae2d6a1a247c2e87608760b28b67be3b778de9
Diego Armando <dgox16@gmail.com>
2024-07-09 10:13:30 -0600

    test

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import { MONGODB_URI } from "./envConfig.js";
 
 export async function connectedDb() {
 	try {
-		const db = await mongoose.connect(MONGODB_URI);
+		const db = await mongoose.connect(MONGODB_URI, { autoIndex: false });
 		console.info("Connected to ", db.connection.name);
 	} catch (error) {
 		console.error(error);
