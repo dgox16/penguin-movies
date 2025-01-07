eb5db4fe790bb39ddb442b428bd939f8bcb52017
Diego Armando <dgox16@gmail.com>
2025-01-06 23:58:04 -0600

    Ahora el año del footer es automatico

--- client/src/components/ui/Footer.jsx
+++ client/src/components/ui/Footer.jsx
@@ -1,21 +1,21 @@
 export const Footer = () => {
-    return (
-        <footer className="mt-5 flex justify-center">
-            <div className="text-center">
-                <p className="py-3 text-text text-sm">
-                    © 2024{" "}
-                    <a
-                        rel="noreferrer"
-                        target="_blank"
-                        href="https://portfolio-alpha-three-66.vercel.app/"
-                    >
-                        Diego Armando Gómez Martínez.
-                    </a>
-                    <span className="hidden sm:inline"> | </span>
-                    <br aria-hidden="true" className="block sm:hidden" />
-                    Casi todos los derechos reservados.
-                </p>
-            </div>
-        </footer>
-    );
+	return (
+		<footer className="mt-5 flex justify-center">
+			<div className="text-center">
+				<p className="py-3 text-text text-sm">
+					© {new Date().getFullYear()}
+					<a
+						rel="noreferrer"
+						target="_blank"
+						href="https://portfolio-alpha-three-66.vercel.app/"
+					>
+						Diego Armando Gómez Martínez.
+					</a>
+					<span className="hidden sm:inline"> | </span>
+					<br aria-hidden="true" className="block sm:hidden" />
+					Casi todos los derechos reservados.
+				</p>
+			</div>
+		</footer>
+	);
 };
